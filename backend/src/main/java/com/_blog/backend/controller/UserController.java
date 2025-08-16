fa2653ee6b896f73d6ba226739791a692a8cce49
saljaoui <soufian2022ff@gmail.com>
2025-08-16 11:35:03 +0100

    add user just for test

---
+++ backend/src/main/java/com/_blog/backend/controller/UserController.java
@@ -0,0 +1,29 @@
+package com._blog.backend.controller;
+
+import com._blog.backend.model.User;
+import com._blog.backend.repository.UserRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+@CrossOrigin
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+    @PostMapping
+    public User addUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+}

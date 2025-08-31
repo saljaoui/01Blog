4b44413794fb52a14b2eb1b079e94c6e103a1a79
saljaoui <soufian2022ff@gmail.com>
2025-08-31 19:06:45 +0100

    fix some bugs frontend

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { Login } from './features/auth/login/login';
 import { Register } from './features/auth/register/register';
+import { Dashboard } from './features/dashboard/dashboard';
 
 export const routes: Routes = [
     { path:'login', component:Login },
-    { path:'register', component:Register }
-
+    { path:'register', component:Register },
+    { path:'', component:Dashboard }
 ];

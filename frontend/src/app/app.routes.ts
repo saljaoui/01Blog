6ab9347b2d0958319c980f7723e4e2f79f1dc5f0
saljaoui <soufian2022ff@gmail.com>
2025-09-01 17:32:37 +0100

    add folders

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from './features/dashboard/dashboard';
+import { Login } from './features/auth/login/login';
+import { Register } from './features/auth/register/register';
+import { Admin } from './features/admin/admin';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", component: Dashboard },
+    { path: "login", component: Login },
+    { path: "register", component: Register },
+    { path: "admin", component: Admin },
+];

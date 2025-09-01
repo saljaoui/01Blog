dfca06311d979d162ce5cbd7405c97dc3b84153d
saljaoui <soufian2022ff@gmail.com>
2025-09-01 13:11:56 +0100

    add tailwind

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,3 @@
 import { Routes } from '@angular/router';
-import { Login } from './features/auth/login/login';
-import { Register } from './features/auth/register/register';
-import { Dashboard } from './features/dashboard/dashboard';
 
-export const routes: Routes = [
-    { path:'login', component:Login },
-    { path:'register', component:Register },
-    { path:'', component:Dashboard }
-];
+export const routes: Routes = [];
---
+++ frontend/tailwind.config.cjs
@@ -0,0 +1,7 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts,scss,css}"],
+  darkMode: 'class',
+  theme: { extend: {} },
+  plugins: [],
+};

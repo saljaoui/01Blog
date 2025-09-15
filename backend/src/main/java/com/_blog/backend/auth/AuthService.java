d4724b01924f88345174cdf638e4d88b4b756c10
saljaoui <soufian2022ff@gmail.com>
2025-09-15 14:27:01 +0100

    add exception

--- backend/src/main/java/com/_blog/backend/auth/AuthService.java
+++ backend/src/main/java/com/_blog/backend/auth/AuthService.java
@@ -1,11 +1,8 @@
 package com._blog.backend.auth;
 
 import java.util.Optional;
-import org.aspectj.weaver.ast.Var;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
 import com._blog.backend.auth.dto.AuthResponse;
 import com._blog.backend.exception.UserAlreadyExistsException;

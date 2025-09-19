db2da795437d1874db0938e35c559192f9e59558
saljaoui <soufian2022ff@gmail.com>
2025-09-19 17:09:11 +0100

    add rote create-post

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { Register } from './features/auth/register/register';
 import { Admin } from './features/admin/admin';
 import { MyProfile } from './features/profile/my-profile/my-profile';
 import { UserProfile } from './features/profile/user-profile/user-profile';
+import { CreatePost } from './features/posts/create-post/create-post';
 
 export const routes: Routes = [
     { path: "", component: Dashboard },
@@ -13,4 +14,5 @@ export const routes: Routes = [
     { path: "admin", component: Admin },
     { path: "my-profile", component: MyProfile },
     { path: "user-profile", component: UserProfile },
+    { path: "create-post", component: CreatePost },
 ];

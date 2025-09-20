08f8c17dcd523f737aef6d100c1345c765e96c5e
saljaoui <soufian2022ff@gmail.com>
2025-09-20 13:11:19 +0100

    fix some css

--- frontend/src/app/features/posts/create-post/create-post.ts
+++ frontend/src/app/features/posts/create-post/create-post.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Header } from '../../../shared/components/header/header';
 
 @Component({
   selector: 'app-create-post',
-  imports: [],
+  imports: [ Header ],
   templateUrl: './create-post.html',
   styleUrl: './create-post.scss'
 })

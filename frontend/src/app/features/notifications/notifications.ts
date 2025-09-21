5fd8ba9ddfd1dec0846a2feb5a38c45ee03a0717
saljaoui <soufian2022ff@gmail.com>
2025-09-21 15:04:31 +0100

    add notifications page

--- frontend/src/app/features/notifications/notifications.ts
+++ frontend/src/app/features/notifications/notifications.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Header } from '../../shared/components/header/header';
+import { ProfileCard } from '../../shared/components/profile-card/profile-card';
 
 @Component({
   selector: 'app-notifications',
-  imports: [],
+  imports: [ Header, ProfileCard ],
   templateUrl: './notifications.html',
   styleUrl: './notifications.scss'
 })

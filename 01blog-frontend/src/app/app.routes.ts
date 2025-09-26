42c2befa8ce342d71ace0a1a11f4ee8573558011
saljaoui <soufian2022ff@gmail.com>
2025-09-26 14:41:40 +0100

    Add new register and new login

--- 01blog-frontend/src/app/app.routes.ts
+++ 01blog-frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Login } from './features/auth/login/login';
+import { Register } from './features/auth/register/register';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'register', component: Register },
+    { path: 'login', component: Login }
+];
---
+++ 01blog-frontend/src/app/features/auth/register/register.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  imports: [],
+  templateUrl: './register.html',
+  styleUrl: '../../../../styles/auth.scss'
+})
+export class Register {
+
+}

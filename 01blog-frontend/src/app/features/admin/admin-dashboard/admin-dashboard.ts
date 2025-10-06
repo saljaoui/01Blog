fa0b85486f3cb5e0098806c9b1055c3f3d1e7537
saljaoui <soufian2022ff@gmail.com>
2025-10-06 13:33:27 +0100

    fis nav for admin

--- 01blog-frontend/src/app/features/admin/admin-dashboard/admin-dashboard.ts
+++ 01blog-frontend/src/app/features/admin/admin-dashboard/admin-dashboard.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { SidebarLeft } from '../../../components/sidebar-left/sidebar-left';
 
 @Component({
   selector: 'app-admin-dashboard',
-  imports: [],
+  imports: [SidebarLeft],
   templateUrl: './admin-dashboard.html',
   styleUrl: './admin-dashboard.scss'
 })

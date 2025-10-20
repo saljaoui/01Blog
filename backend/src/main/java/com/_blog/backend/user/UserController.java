46a1a4f32443b7365144054ea557b43f83ad0941
saljaoui <soufian2022ff@gmail.com>
2025-10-20 21:42:30 +0100

    fix refrechToken and jwt token

--- backend/src/main/java/com/_blog/backend/user/UserController.java
+++ backend/src/main/java/com/_blog/backend/user/UserController.java
@@ -3,8 +3,6 @@ package com._blog.backend.user;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 

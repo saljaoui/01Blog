a5e677ec4750af4262c4be1cc1381b2a644087af
saljaoui <soufian2022ff@gmail.com>
2025-11-04 18:12:59 +0100

    `Added input validation and error handling in login and registration forms, updated authentication service to handle bad credentials, and added popup notifications for login and registration results.`

--- backend/src/main/java/com/_blog/backend/follow/FollowController.java
+++ backend/src/main/java/com/_blog/backend/follow/FollowController.java
@@ -5,7 +5,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-import java.util.List;
 import java.util.UUID;
 
 @RestController
